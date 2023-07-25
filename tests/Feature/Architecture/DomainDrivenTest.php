b89e1f92dd4ea874a2bd188d5e39d90b916a7c44
Alejandro Velez-Calderon <alejandrovelezcalderon@gmail.com>
2023-07-25 10:06:29 -0400

    feat: domain driven tests

--- tests/Feature/Architecture/DomainDrivenTest.php
+++ tests/Feature/Architecture/DomainDrivenTest.php
@@ -1,11 +1,19 @@
 <?php
 
-
 describe('App\Domains\Support', function () {
-    test('boundaries')
-        ->expect('App\Domains\Support\Services')
-        ->toUse('App\Domains\Support\Contracts')
-        ->not
-        ->toUse('App\Domains\Sales\Services');
-});
+    describe('external boundaries', function () {
+        test('models')
+            ->expect('App\Domains\Support\Models')
+            ->toOnlyBeUsedIn('App\Domains\Support');
 
+        test('services')
+            ->expect('App\Domains\Support\Services')
+            ->toOnlyBeUsedIn('App\Domains\Support');
+    });
+
+    test('internal boundaries')
+        ->expect('App\Domains\Support\Contracts')
+        ->toUseNothing()
+        ->ignoring('Spatie\LaravelData\Data')
+        ->ignoring('Spatie\LaravelData\Attributes\DataCollectionOf');
+});
